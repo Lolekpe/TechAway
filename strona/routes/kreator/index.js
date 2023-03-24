67a07714f70b79bb23f0e20596a0e5a22311df66
Patryczek <49070208+Lolekpe@users.noreply.github.com>
2023-03-24 19:43:02 +0100

    no dobra

--- strona/routes/kreator/index.js
+++ strona/routes/kreator/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var sql = require("mysql");
-var database = [];
+var database = {};
 var con = sql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -12,52 +12,32 @@ var con = sql.createConnection({
 
 app.route('/')
     .get((req, res, next) => {
-        if(!req.query.krok) {
-            var stage1 = `<div class="wybor-nazwy">` +
-                `<div class="tekst-wyboru-nazwy"><p>Podaj nazwę sklepu</p></div>` +
-                `<div class="tekst-rozszerzenie-nazwy"><p>Jak ma się nazywać twój sklep?</p></div>` +
-                `<div class="formularz-wpisania-nazwy"><form action="/kreator?krok=1" method="post"><span class="ikona"><i class="fa-solid fa-signature"></i></span>` +
-                `<input type="text" name="nazwa" required autocomplete="off"><p><button>Dalej <i class="fa-solid fa-arrow-right"></i></button></p></form></div>` +
-                `<div class="anuluj"><span>Anuluj</span></div></div>`;
-    
-    
-            return res.render('kreator/index.ejs', { stage: "TechAway | Kreator strony", wyglad: stage1 })
-        }
-        if(req.query.krok == 2 && req.query.opcja) {
-            req.session.current_query = req.query.opcja;
-            return res.redirect("/kreator?krok=3");
-
+        if (!req.query.krok) {
+            return res.render('kreator/index.ejs', { stage: "TechAway | Kreator strony", wyglad: 1 })
         }
         switch (req.query.krok) {
-            case '2': 
-                var stage2 = `<div class="wybur-motywu"><div class="left-motyw"><div class="napis-wybieranie-motywu">` +
-                `<div class="napiski-container"><p class="napisjedenuwu">Wybierz motyw swojej strony</p>` + 
-                `<p class="napisdwauwu">Wybierz w jakim motywie będzie robiona twoja strona, zadecyduj o jej wyglądzie końcowym.</p></div></div>` + 
-                `</div><div class="right-motyw"><div class="motywy-container"><div class="motywy"><div class="motywy-left">` +
-                `<a href="/kreator?krok=2&opcja=1"><div class="motyw-jest-uwu motywjeden motwy-podwujny">` + 
-                `<div style="background-color: white; border-top-left-radius: 5px; border-top-right-radius: 5px;"></div>` + 
-                `<div style="background-color: black; border-bottom-left-radius: 5px; border-bottom-right-radius: 5px;"></div></div></a>` + 
-                `<a href="/kreator?krok=2&opcja=2"><div class="motyw-jest-uwu motywdwa">` +
-                `<div style="background-color: #CB997E; border-top-left-radius: 5px; border-top-right-radius: 5px;"></div>` + 
-                `<div style="background-color: #A5A58D;"></div><div style="background-color: #F0EFEB; border-bottom-left-radius: 5px; border-bottom-right-radius: 5px;"></div>` + 
-                `</div></a></div><div class="motywyprawo"><a href="/kreator?krok=2&opcja=3"><div class="motyw-jest-uwu motywtrzy">` + 
-                `<div style="background-color:  #FAD2E1; border-top-left-radius: 5px; border-top-right-radius: 5px;"></div>` + 
-                `<div style="background-color: #BEE1E6;"></div><div style="background-color: #FFFFFC; border-bottom-left-radius: 5px; border-bottom-right-radius: 5px;"></div>` + 
-                `</div></a><a href="/kreator?krok=2&opcja=4"><div class="motyw-jest-uwu motywcztery">` + 
-                `<div  style="background-color:  #4A2419; border-top-left-radius: 5px; border-top-right-radius: 5px;"></div>` + 
-                `<div style="background-color: #343434;"></div><div style="background-color: #006466; border-bottom-left-radius: 5px; border-bottom-right-radius: 5px;"></div>` + 
-                `</div></a></div></div></div></div><div class="anuluj"><span>Anuluj</span></div></div>`;
+            case '2':
+                return res.render('kreator/index.ejs', { stage: "TechAway | Kreator strony", wyglad: 2 })
+                
+            case '3':
+                if (database.motyw) {
+                    database.motyw = req.query.opcja;
+                    return res.render('kreator/index.ejs', { stage: "TechAway | Kreator strony", wyglad: 3 });
+                }
+                database.motyw = req.query.opcja;
 
-                return res.render('kreator/index.ejs', { stage: "TechAway | Kreator strony", wyglad: stage2 })
-            case '3': 
-                console.log(req.session.current_query);
-                var stage3 = `<div class="ulozeniestrony-wybor"><div class="left-ulozenie">` + 
-                `<div class="napis-wybieranie-motywu"><div class="napiski-container"><p class="napisjedenuwu">Wybierz ułożenie swojej strony</p>` + 
-                `<p class="napisdwauwu">Wybierz styl ułożenia swojej strony tak aby jak najbardziej przypadał ci do gustu.</p></div></div>` + 
-                `</div><div class="right-ulozenie"><div class="elementy"><div class="ulozenie-sportowe ulozeniejeden"></div>` + 
-                `<div class="ulozenie-sportowe ulozeniedwa"></div></div></div><div class="anuluj"><span>Anuluj</span></div></div>`;
+                return res.render('kreator/index.ejs', { stage: "TechAway | Kreator strony", wyglad: 3 });
+            case '4':
+                if (database.uklad) {
+                    database.uklad = req.query.opcja;
+                    return res.render("kreator/index.ejs", { stage: "TechAway | Kreator strony", wyglad: 4 });
 
-                return res.render('kreator/index.ejs', { stage: "TechAway | Kreator strony", wyglad: stage3 })
+                }
+                database.uklad = req.query.opcja;
+                return res.render("kreator/index.ejs", { stage: "TechAway | Kreator strony", wyglad: 4 });
+            case '5':
+                console.log(database);
+                return res.render("kreator/index.ejs", { stage: "TechAway | Kreator strony", wyglad: 5 })
         }
 
     }).post((req, res) => {
@@ -67,12 +47,25 @@ app.route('/')
                     con.query(`SELECT * FROM sklepy WHERE nazwa LIKE "${req.body.nazwa}"`, (err, row) => {
                         if (err) return res.send(err);
                         if (row.length !== 0) return res.send("No niestety");
-                        
-                        database.push(req.body.nazwa);
+                        if (database.nazwa) {
+                            database.nazwa = req.body.nazwa;
+                            return res.redirect('/kreator?krok=2');
+                        }
+                        database.nazwa = req.body.nazwa;
                         return res.redirect('/kreator?krok=2');
                     })
                 })
                 break;
+            case '4':
+                if (database.opis) {
+                    database.opis = req.body.opis;
+                    return res.redirect('/kreator?krok=5');
+                }
+                database.opis = req.body.opis;
+                return res.redirect('/kreator?krok=5');
+            case '5':
+
+
         }
     });
 
