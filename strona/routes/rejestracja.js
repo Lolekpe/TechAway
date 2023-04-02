007ad904b352b7ce5b37ff6e92069fdff61232c2
Patryczek <49070208+Lolekpe@users.noreply.github.com>
2023-04-02 19:10:20 +0200

    .

--- strona/routes/rejestracja.js
+++ strona/routes/rejestracja.js
@@ -7,16 +7,14 @@ var con = sql.createConnection({
     password: "",
     database: 'techaway'
 })
-
 var resul = 0;
 /* GET home page. */
 app.route('/')
-    .post(function (req, res) {
+.post(function (req, res) {
         var crypto = require("crypto");
         var sha256 = crypto.createHash("sha256");
         sha256.update(req.body.haslo, "utf");
         resul = sha256.digest("base64");
-
         con.connect(function () {
             con.query(`SELECT email FROM uzytkownicy WHERE email = '${req.body.mail}'`, (err, row) => {
                 if (err) return console.log(err);
